c966bf1964e76809a02199a250f665583a767525
Mairbek Khadikov <mairbek@conduit.xyz>
2025-08-01 20:54:28 -0700

    zsh works

--- modules/shell.nix
+++ modules/shell.nix
@@ -10,6 +10,51 @@ in
     yq-go
   ];
 
+  programs.zsh = {
+    enable = true;                 # ← HM now owns ~/.zshrc & friends
+    enableCompletion = true;
+    autocd = true;
+    history = {
+      size  = 10000;
+      ignoreDups = true;
+    };
+
+   "oh-my-zsh" = {
+      enable = true;
+      theme  = "robbyrussell";
+      plugins = [ "git" "asdf" ];
+    };
+
+    initContent = ''
+      # Show “(nix)” in magenta when IN_NIX_SHELL is set
+      function _nix_prompt_prefix {
+        if [[ -n "$IN_NIX_SHELL" ]]; then
+          PROMPT="%{$fg[magenta]%}(nix)%{$reset_color%} $PROMPT_BASE"
+        else
+          PROMPT="$PROMPT_BASE"
+        fi
+      }
+
+      PROMPT_BASE="$PROMPT"
+      autoload -Uz add-zsh-hook
+      add-zsh-hook precmd _nix_prompt_prefix
+    '';
+
+
+    plugins = [
+      {
+        name = "nix-shell";
+        src  = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo  = "zsh-nix-shell";
+          rev   = "master";
+          hash  = "sha256-Rtg8kWVLhXRuD2/Ctbtgz9MQCtKZOLpAIdommZhXKdE=";
+        };
+        file = "plugins/nix-shell/nix-shell.plugin.zsh";
+      }
+    ];
+  };
+
   programs.ssh = {
     enable = true;
 
@@ -30,7 +75,7 @@ in
     enable = true;
     nix-direnv.enable = true;
     # TODO(mairbek): enable when zsh is configured with nix
-    # enableZshIntegration = true;
+    enableZshIntegration = true;
     silent = true;
   };
 
