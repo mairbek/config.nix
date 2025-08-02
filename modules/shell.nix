502937746c105c79857062fef7721a1a8bd2459c
Mairbek Khadikov <mairbek@conduit.xyz>
2025-08-01 20:09:49 -0700

    more stuff enabled

--- modules/shell.nix
+++ modules/shell.nix
@@ -9,4 +9,29 @@ in
     jq
     yq-go
   ];
+
+  programs.ssh = {
+    enable = true;
+
+    # orbstack included
+    includes = [ "~/.orbstack/ssh/config" ];
+
+    # TODO(mairbek): what's the best way to generate ssh keys with nix?
+    matchBlocks."github.com" = {
+      identityFile   = "~/.ssh/id_ed25519";
+      extraOptions = {
+        AddKeysToAgent = "yes";
+        UseKeychain    = "yes";      # macOS-specific
+      };
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    # TODO(mairbek): enable when zsh is configured with nix
+    # enableZshIntegration = true;
+    silent = true;
+  };
+
 }
